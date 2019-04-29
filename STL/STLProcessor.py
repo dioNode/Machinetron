83f47d84b4554dbd8f3465fa3b701272094ade59
Dion Lao <dion.lao@uqconnect.edu.au>
2019-04-29 14:40:12 +1000

    Linked drill detection to trigger drill command

--- STL/STLProcessor.py
+++ STL/STLProcessor.py
@@ -12,13 +12,41 @@ class STLProcessor:
         # generateSlices(filename)
         outPath = "STL\output"
         path = "STL\output"
+        commands = []
 
         # iterate through the names of contents of the folder
+        totalDrillPoints = []
         for image_path in os.listdir(path):
             # create the full input path and read the file
             input_path = os.path.join(path, image_path)
             img = cv2.imread(input_path, 0)
-            detectDrill(img)
+            drillPoints = detectDrill(img)
+            totalDrillPoints.append(drillPoints)
 
+        sliceDepth = 10
+        totalDrillPoints.reverse()
 
+        trackingPoints = []
 
+        print(totalDrillPoints)
+
+        backwardsDrillHoleLengths = {}
+
+        # Get initial holes from surface
+        for drillPoint in totalDrillPoints[0]:
+            backwardsDrillHoleLengths[drillPoint] = 0
+            trackingPoints.append(drillPoint)
+
+        for drillPoints in totalDrillPoints:
+            updatedTrackingPoints = []
+            for trackedPoint in trackingPoints:
+                if trackedPoint in drillPoints:
+                    backwardsDrillHoleLengths[trackedPoint] += sliceDepth
+                    updatedTrackingPoints.append(trackedPoint)
+            trackingPoints = updatedTrackingPoints
+
+        for drillPoint, depth in backwardsDrillHoleLengths.items():
+            foamWidth = controller.xLength
+            foamHeight = controller.zLength
+            print('test', drillPoint, depth)
+            controller.commandGenerator.drill('front', drillPoint[0]*foamWidth - foamWidth/2, drillPoint[1]*foamHeight, depth)
