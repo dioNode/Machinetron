f2acaf7c8d9e6e85ac14576fa3788091849861d0
Dion Lao <dion.lao@uqconnect.edu.au>
2019-05-21 21:50:04 +1000

    Added code for resetting current command with button

--- CircuitComponents/GoButton.py
+++ CircuitComponents/GoButton.py
@@ -9,17 +9,26 @@ class GoButton:
         GPIO.setup(37, GPIO.IN,
                    pull_up_down=GPIO.PUD_DOWN)  # Set pin 37 to be an input pin and set initial value to be pulled low (off)
         GPIO.add_event_detect(37, GPIO.RISING, callback=self.buttonClicked)  # Setup event on pin 37 rising edge
+        GPIO.add_event_detect(37, GPIO.FALLING, callback=self.buttonReleased)  # Setup event on pin 37 rising edge
         self.depressed = False
         self.controller = controller
         self.lastPressedTime = 0
+        self.pushDownTime = 0
 
     def buttonClicked(self, channel):
         currentTime = time.time()
 
         if currentTime - self.lastPressedTime > 1:
             self.lastPressedTime = currentTime
-            print('pressed')
-            self.controller.goButtonClicked()
+            self.pressDownTime = currentTime
 
+    def buttonReleased(self, channel):
+        releaseTime = time.time()
+        if releaseTime - self.lastPressedTime < 2:
+            print('normal press')
+            self.controller.goButtonClicked()
+        else:
+            print('long press')
+            self.controller.resetCurrentCommand()
 
 
